939090c32ed9d1204aef33345c74ec05ba102605
VShybanov <vs.shibanov@gmail.com>
2023-10-11 12:18:17 +0300

    commit

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,6 +1,6 @@
 public class Lesson4 {
     public static void main(String[] args) {
         System.out.println("First commit for Git.");
-        System.out.println("2nd commit for Git. V");        
+        System.out.println("2nd commit for Git.");
     }
 }
